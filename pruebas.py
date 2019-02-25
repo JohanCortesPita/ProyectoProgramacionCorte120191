0748a0e7b00815995b30931808f841b067417bfe
Johan Andres Cortes Pita <jcortes71058@universidadean.edu.co>
2019-02-25 00:49:20 -0500

    Se realizan pruebas unitarias de las funciones 1,2,3 y 4

--- pruebas.py
+++ pruebas.py
@@ -8,15 +8,32 @@ class pruebas(unittest.TestCase):
     def test_calcular_precio_producto(self):
         self.assertEqual(f.calcular_precio_producto(1000), 1500)
         self.assertEqual(f.calcular_precio_producto(0), 0)
+        self.assertEqual(f.calcular_precio_producto(500), 750)
+        self.assertEqual(f.calcular_precio_producto(-100), 'El valor debe ser mayor que 0')
+        self.assertEqual(f.calcular_precio_producto(-50), 'El valor debe ser mayor que 0' )
 
     def test_calcular_precio_servicio(self):
-        pass
+        self.assertEqual(f.calcular_precio_servicio(1), 100000)
+        self.assertEqual(f.calcular_precio_servicio(5), 500000)
+        self.assertEqual(f.calcular_precio_servicio(0), 0)
+        self.assertEqual(f.calcular_precio_servicio(-1), 'Las horas trabajadas deben ser mayores de 0')
+        self.assertEqual(f.calcular_precio_servicio(-5), 'Las horas trabajadas deben ser mayores de 0')
 
     def test_calcular_precio_servicio_extras(self):
-        pass
+        self.assertEqual(f.calcular_precio_servicio_extras(10), 1250000)
+        self.assertEqual(f.calcular_precio_servicio_extras(9), 1125000)
+        self.assertEqual(f.calcular_precio_servicio_extras(1), 125000)
+        self.assertEqual(f.calcular_precio_servicio_extras(0), 0)
+        self.assertEqual(f.calcular_precio_servicio_extras(-1), 'La horas trabajadas deben ser mayores a 0')
+        self.assertEqual(f.calcular_precio_servicio_extras(-5), 'La horas trabajadas deben ser mayores a 0')
 
     def test_calcular_costo_envio(self):
-        pass
+        self.assertEqual(f.calcular_costo_envio(1), 115)
+        self.assertEqual(f.calcular_costo_envio(2), 230)
+        self.assertEqual(f.calcular_costo_envio(0), 0)
+        self.assertIsNot(f.calcular_costo_envio(-1), 'No es valido')
+        self.assertIsNot(f.calcular_costo_envio(-2), 'No es valido')
+
 
     def test_calcular_precio_producto_fuera(self):
         pass
