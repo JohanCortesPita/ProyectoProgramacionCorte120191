f231adbb3e6836cdd26f7d3aeb1fc03c7dfe3eb6
David <wvillar85769@universidadean.edu.co>
2019-02-25 23:00:23 -0500

    Se realizan pruebas unitarias numero 9, 10, 11 y 12

--- pruebas.py
+++ pruebas.py
@@ -31,17 +31,29 @@ class pruebas(unittest.TestCase):
         pass
 
     def test_calcular_iva_servicio_fuera(self):
-        pass
+        self.assertEqual(f.calcular_iva_servicio_fuera(10,16),160000.0)
+        self.assertEqual(f.calcular_iva_servicio_fuera(-4,-48),"Cifras no validas")
+        self.assertEqual(f.calcular_iva_servicio_fuera(-8,98),"Cifras no validas")
+        self.assertEqual(f.calcular_iva_servicio_fuera(-8,89),"Cifras no validas")
 
     def test_calcular_recaudo_locales(self):
-        pass
+        self.assertEqual(f.calcular_recaudo_locales(1,1,1),4.5)
+        self.assertEqual(f.calcular_recaudo_locales(60,20,30),165.0)
+        self.assertEqual(f.calcular_recaudo_locales(20.5,15,9.5),67.5)
+        self.assertEqual(f.calcular_recaudo_locales(20.89,854,-85),"Cifras no validas")
 
     def test_calcular_recaudo_horas_extra(self):
-        pass
+        self.assertEqual(f.calcular_recaudo_horas_extra(2,6,8,4),2000000)
+        self.assertEqual(f.calcular_recaudo_horas_extra(6,4,2,1),1300000)
+        self.assertEqual(f.calcular_recaudo_horas_extra(5,4,3,-85),"Cifras no validas")
+        self.assertEqual(f.calcular_recaudo_horas_extra(-5,-54,-98,-68),"Cifras no vlidas")
+
 
     def test_calcular_recaudo_mixto_local(self):
-        pass
-
+        self.assertEqual(f.calcular_recaudo_mixto_local(100,300,4,8),1200600.0)
+        self.assertEqual(f.calcular_recaudo_mixto_local(5000,3500,8,8),1612750.0)
+        self.assertEqual(f.calcular_recaudo_mixto_local(-100,50,-85,58),"Cifras no validas")
+        self.assertEqual(f.calcular_recaudo_mixto_local(5,98,4,8),"Cifras no validas")
 
 if __name__ == 'main':
     unittest.main()
